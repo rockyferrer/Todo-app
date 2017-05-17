ad85fcf2c5081b99b2ec3b0a4605a3e2352eef4e
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-16 20:15:24 -0400

    add static AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+
+        var todo = this.refs.todo.value;
+        if (todo.length > 0) {
+            this.refs.todo.value = '';
+            this.props.onAddNewTodo(todo);
+        } else {
+            this.refs.todo.focus();
+        }
+    },
+
+    render: function() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" ref="todo" placeholder="Enter task..."/>
+                    <button className="button expanded">Add Task</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+module.exports = AddTodo;

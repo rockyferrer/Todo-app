75591e58832a63d9c36967dac95a661ce9b4c96b
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-18 17:32:29 -0400

    wired AddTodo component and added tests

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,14 @@ describe('TodoApp', () => {
     it('should exist', () => {
         expect(TodoApp).toExist();
     });
+
+    it('should add todo to the todos state on handleAddTodo', () => {
+        var task = 'Random task';
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
+        todoApp.setState({ todos: []});
+        todoApp.handleAddTodo(task);
+
+        expect(todoApp.state.todos[0].text).toBe(task);
+    });
 });

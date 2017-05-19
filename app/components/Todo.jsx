43f26a418c22f58f2969cfb5c13d3d97c5384068
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-19 15:24:48 -0400

    Added timestamps for todos using moment package

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,14 +1,30 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
     render: function() {
-        var {id, text, completed} = this.props;
+        var {id, text, completed, createdAt, completedAt} = this.props;
+        var renderCreatedDate = () => {
+            var timestamp = createdAt;
+
+            return 'Created ' + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+        }
+
+        var renderCompletedDate = () => {
+            if (completed && completedAt) {
+                var timestamp = completedAt;
+                return 'Completed ' + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+            }
+        }
+
         return (
             <div onClick={() => {
                 this.props.onToggle(id);
             }}>
                 <input type="checkbox" checked={completed} />
-                {text}
+                <p>{text}</p>
+                <p>{renderCreatedDate()}</p>
+                <p>{renderCompletedDate()}</p>
             </div>
         );
     }

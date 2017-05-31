679dce5ff09adbed622697ca1cf4fd261b06430d
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-31 19:52:29 -0400

    Move firebase credentials to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyB3gAMNM84ZVsOJ2m1su_fYk8eDR4JQ9E0",
-        authDomain: "rocky-todo-app.firebaseapp.com",
-        databaseURL: "https://rocky-todo-app.firebaseio.com",
-        projectId: "rocky-todo-app",
-        storageBucket: "rocky-todo-app.appspot.com",
-        messagingSenderId: "377736971666"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        storageBucket: process.env.STORAGE_BUCKET,
     };
     firebase.initializeApp(config);
 } catch(e) {

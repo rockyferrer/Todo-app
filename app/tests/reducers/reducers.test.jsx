1c40ad00c89ed001cf51131e2a3453c3181d3613
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-31 14:48:01 -0400

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -41,7 +41,7 @@ describe('Reducers', () => {
             expect(res[0]).toEqual(action.todo);
         });
 
-        it('should toggle todo', () => {
+        it('should update todo', () => {
             var todos = [
                 {
                     id: '2',
@@ -51,13 +51,19 @@ describe('Reducers', () => {
                     completedAt: 456
                 }
             ];
+            var updates = {
+                completed: false,
+                completedAt: null
+            }
             var action = {
-                type: 'TOGGLE_TODO',
-                id: '2'
+                type: 'UPDATE_TODO',
+                id: todos[0].id,
+                updates
             };
             var res = reducers.todosReducer(df(todos), df(action));
-            expect(res[0].completed).toEqual(false);
-            expect(res[0].completedAt).toEqual(undefined);
+            expect(res[0].completed).toEqual(updates.completed);
+            expect(res[0].completedAt).toEqual(updates.completedAt);
+            expect(res[0].text).toEqual(todos[0].text);
         });
 
         it('should add existing todos', () => {
